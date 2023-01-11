3df2d5d87ef7cb8aabd75dfa9f04cbd8a88683aa
Joao Fleming <joao.fleming@ltm.digital>
2023-01-11 15:24:08 -0300

    diretivas ngif e bootstrap

---
+++ src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-diretiva-ngif',
+  templateUrl: './diretiva-ngif.component.html',
+  styleUrls: ['./diretiva-ngif.component.css']
+})
+export class DiretivaNgifComponent {
+  cursos: string [] = ['angular']
+  mostraCursos: boolean = false
+
+  onMostrarCurso(){
+    this.mostraCursos = !this.mostraCursos
+  }
+}
